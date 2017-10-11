f525a6dbdab0483fcaf6b4da3b0b4dd0b6e0ea57
rubends <rubendeswaef@gmail.com>
2017-10-11 11:34:39 +0200

    created tutorialserver

--- Labo2/Server/src/compute/Compute.java
+++ Labo2/Server/src/compute/Compute.java
@@ -1,9 +1,7 @@
 package compute;
 
-        import javafx.concurrent.Task;
-
-        import java.rmi.Remote;
-        import java.rmi.RemoteException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Compute extends Remote {
     <T> T executeTask(Task<T> t) throws RemoteException;
