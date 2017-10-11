305042e1b227cea5c7f823e41e7ab46b08dd7529
Imre Liessens <imre.liessens@telenet.be>
2017-10-11 20:00:22 +0200

    Client usability improvements

--- Labo2/Client/src/be/ua/bank/UserInterface.java
+++ Labo2/Client/src/be/ua/bank/UserInterface.java
@@ -1,5 +1,6 @@
 package be.ua.bank;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -27,22 +28,31 @@ public class UserInterface {
             System.out.println("\t 4. Exit bank");
             System.out.print("> ");
 
-            int selection = input.nextInt();
-            switch (selection) {
-                case 1:
-                    getBalance();
-                    break;
-                case 2:
-                    deposit();
-                    break;
-                case 3:
-                    withdraw();
-                    break;
-                case 4:
-                    return;
-                default:
-                    System.out.println("Please enter a valid option.");
+            try {
+                int selection = input.nextInt();
+                switch (selection) {
+                    case 1:
+                        getBalance();
+                        break;
+                    case 2:
+                        deposit();
+                        break;
+                    case 3:
+                        withdraw();
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("Please enter a valid option.");
+                }
             }
+            catch (InputMismatchException e) {
+                // clear buffer
+                input.nextLine();
+                System.out.println("Please enter only numbers.");
+            }
+
+            System.out.println("--\n");
         }
     }
 
