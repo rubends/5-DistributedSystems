009c3d268a7272bc6ddba852d541f3ccdde39da4
Imre Liessens <imre.liessens@telenet.be>
2017-10-11 12:16:56 +0200

    Securitymanager fixed

--- Labo2/Client/src/be/ua/bank/UserInterface.java
+++ Labo2/Client/src/be/ua/bank/UserInterface.java
@@ -4,18 +4,66 @@ import java.util.Scanner;
 
 public class UserInterface {
     private Scanner input;
+    private Bank bank;
 
     public UserInterface() {
         input = new Scanner(System.in);
         System.out.println("Welcome to the bank");
-        selectOption();
+        setup();
+        startUI();
     }
 
-    public void selectOption() {
-        System.out.println("Choose an option:\n\t 1. View balance");
-        System.out.println("\t 2. Desposit money");
-        System.out.println("\t 3. Withdraw money");
+    private void setup() {
+            // Eventueel vragen aan gebruiker
+            RMIConnector connector =new  RMIConnector("localhost");
+            bank = connector.getBank();
+    }
 
-       int selection =  input.nextInt();
+    public void startUI() {
+        while(true) {
+            System.out.println("Choose an option:\n\t 1. View balance");
+            System.out.println("\t 2. Desposit money");
+            System.out.println("\t 3. Withdraw money");
+            System.out.println("\t 4. Exit bank");
+            System.out.print("> ");
+
+            int selection = input.nextInt();
+            switch (selection) {
+                case 1:
+                    getBalance();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Please enter a valid option.");
+            }
+        }
+    }
+
+    private void  getBalance() {
+       float balance = bank.getBalance();
+       System.out.println("Current balance: "+balance);
+    }
+
+    private void deposit() {
+        System.out.print("Please enter amount to deposit: ");
+        bank.deposit(input.nextFloat());
+    }
+
+    private void withdraw() {
+        System.out.print("Please enter amount to withdraw: ");
+        float amount = input.nextFloat();
+        if(bank.withdraw(amount)) {
+            System.out.println("Withdrawal succeeded");
+        }
+        else {
+            System.out.println("Withdrawal failed. Balance too low");
+        }
     }
 }
